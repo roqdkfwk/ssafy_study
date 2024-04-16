c00222bdd7c70a4424aaa8cfa7670095f3567843
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2024-04-16 14:44:09 +0900

    [Gold V] Title: 벼락치기, Time: 144 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -8,7 +8,7 @@ public class Main {
 	static int N, T;	// 단원 개수, 총 시간
 	static int[] K;	// 단원 별 예상 공부 시간
 	static int[] S;	// 문제 배점
-	static int[][] DP;
+	static int[] DP;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,18 +27,13 @@ public class Main {
 			S[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		DP = new int[N + 1][T + 1];
+		DP = new int[T + 1];
 		for (int i = 1; i < N + 1; i++) {
-			for (int k = 0; k <= T; k++) {
-				
-				if (k >= K[i]) {
-					DP[i][k] = Math.max(DP[i - 1][k], DP[i - 1][k - K[i]] + S[i]);
-				} else {
-					DP[i][k] = DP[i - 1][k];
-				}
+			for (int k = T; k >= K[i]; k--) {
+				DP[k] = Math.max(DP[k], DP[k - K[i]] + S[i]);
 			}
 		}	// i에 대한 for문
 		
-		System.out.println(DP[N][T]);
+		System.out.println(DP[T]);
 	}	// main
 }
