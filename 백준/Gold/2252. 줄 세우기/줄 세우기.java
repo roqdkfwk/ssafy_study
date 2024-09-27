62c63ecdb14e9e0072f57efe94446fcd534a9f1f
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2024-09-27 23:27:06 +0900

    [Gold III] Title: 줄 세우기, Time: 452 ms, Memory: 52012 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] degree;
+	static List<List<Integer>> adj;
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList<>();
+        degree = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+        	adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            adj.get(A).add(B);
+            degree[B]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (degree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sb.append(current).append(" ");
+
+            for (int next : adj.get(current)) {
+            	degree[next]--;
+                if (degree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
