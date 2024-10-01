c272f9bf4686dc4cf97e94a120fce7461d71fc32
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2024-10-01 13:00:02 +0900

    [level 3] Title: 입국심사, Time: 42.89 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,42 +1,27 @@
-import java.util.*;
-
 class Solution {
-    
-    int n, size;
-    int[] times;
-    
     public long solution(int n, int[] times) {
-        this.n = n;
-        this.times = times;
-        this.size = times.length;
+        // 1. left는 0, answer는 Max
+        long left = 0, answer = Long.MAX_VALUE;
         
-        long left = 0;
-        long right = Long.MAX_VALUE;
-        while (left < right) {
-            long mid = left + (right - left) / 2;
-            if (calculate(mid)) {
-                right = mid;
+        // 2. 이분탐색으로 특정 시간에 심사관별로 심사할 수 있는 인원의 총합을 구함
+        while (left < answer) {
+            long mid = left + (answer - left) / 2;
+            long count = 0;  // 입국 심사를 받을 수 있는 사람들의 합
+            
+            for (int i = 0; i < times.length; i++) {
+                count += mid / times[i];
+                if (count >= n) {
+                    break;
+                }
+            }
+            
+            if (count >= n) {
+                answer  = mid;
             } else {
                 left = mid + 1;
             }
         }
-        
-        return right;
-    }
-    
-    private boolean calculate(long MID) {
-        long count = 0;  // 심사할 수 있는 사람 수
-        for (int i = 0; i < size; i++) {
-            count += (MID / times[i]); // MID분이 지났을 때 i번째 심사관이 심사할 수 있는 사람 수를 모두 더함
-            if (count >= n) {
-                return true;
-            }
-        }
-        
-        if (count < n) {
-            return false;
-        }
-        
-        return true;
+        // 3. 구한 값 중 가장 작은 값을 찾음
+        return answer;
     }
 }
