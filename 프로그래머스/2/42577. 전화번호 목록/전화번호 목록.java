269e60ffccc79f0d71c0f28a8afcb7f0cf780a45
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2024-10-05 17:53:17 +0900

    [level 2] Title: 전화번호 목록, Time: 292.73 ms, Memory: 245 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,35 +1,16 @@
 import java.util.*;
 class Solution {
-    
-    class Node {
-        Map<Character, Node> child = new HashMap<>();
-        boolean isEnd;
-        
-        public Node () {
-            this.isEnd = false;
-        }
-    }
-    
     public boolean solution(String[] phone_book) {
-        Node root = new Node();
-        for (String str : phone_book) {
-            Node node = root;
-            
-            for (int j = 0; j < str.length(); j++) {
-                char key = str.charAt(j);
-                
-                if (node.isEnd) {  // 현재 노드가 이미 어떤 번호의 끝이라면
-                    return false;  // 현재 삽입 중인 번호는 기존 번호의 접두어
-                }
-                
-                node.child.putIfAbsent(key, new Node());
-                node = node.child.get(key);
-                
-                if (j == str.length() - 1) {        // 현재 삽입 중인 번호의 마지막 문자
-                    if (!node.child.isEmpty()) {    // 이미 다른 번호가 현재 번호로 시작한다면
-                        return false;               // 현재 번호는 다른 번호의 접두어
-                    }
-                    node.isEnd = true;              // 현재 번호의 끝을 표시
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            String key = phone_book[i];
+            map.put(key, 1);
+        }
+        for (int i = 0; i < phone_book.length; i++) {
+            String key = phone_book[i];
+            for (int j = 1; j < key.length(); j++) {
+                if (map.containsKey(key.substring(0, j))) {
+                    return false;
                 }
             }
         }
