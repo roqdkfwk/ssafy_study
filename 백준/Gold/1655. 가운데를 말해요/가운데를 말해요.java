1f7e89fc01fa6ab089789ef6b0d021f0c26e9668
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2024-10-29 15:54:21 +0900

    [Gold II] Title: 가운데를 말해요, Time: 1064 ms, Memory: 39872 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    static int N;
+    static PriorityQueue<Integer> pre, post;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        pre = new PriorityQueue<>(Comparator.reverseOrder());
+        post = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {   // 짝수일 땐 pre
+                pre.add(Integer.parseInt(br.readLine()));
+            } else {            // 홀수일 땐 post
+                post.add(Integer.parseInt(br.readLine()));
+            }
+
+            if (!pre.isEmpty() && !post.isEmpty() && pre.peek() > post.peek()) {
+                int tmp = pre.poll();
+                pre.add(post.poll());
+                post.add(tmp);
+            }
+
+            System.out.println(pre.peek());
+        }
+    }
+}
