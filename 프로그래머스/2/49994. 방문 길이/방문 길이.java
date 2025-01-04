5ff3cb40d4731d5fe688b808d187af55c872f97a
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-01-04 21:59:50 +0900

    [level 2] Title: 방문 길이, Time: 16.41 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Solution {
+    
+    int r, c;
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
+    HashMap<String, Integer> map1, map2;
+    int answer;
+    
+    public int solution(String dirs) {
+        this.map1 = new HashMap<>();
+        this.map2 = new HashMap<>();
+        
+        for (int n = 0; n < dirs.length(); n++) {
+            move(dirs.charAt(n));
+        }
+        return map1.size();
+    }
+    
+    void move(char direction) {
+        // U : 1, D : 1, L : 2, R : 3
+        int d = 0;
+        if (direction == 'U') d = 1;
+        else if (direction == 'D') d = 0;
+        else if (direction == 'L') d = 2;
+        else d = 3;
+        
+        int nr = r + dr[d];
+        int nc = c + dc[d];
+        
+        if (nr > 5 || nr < -5 || nc > 5 || nc < -5) return;
+        
+        double cr = (double)(r + nr) / 2;
+        double cc = (double)(c + nc) / 2;
+        
+        r = nr;
+        c = nc;
+        
+        String key = cr + "." + cc;
+        if (map1.containsKey(key)) map2.put(key, 1);
+        else map1.put(key, 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
