58833a509850d77ee408d5e0435f4976163bd3b5
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-01-23 18:31:59 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28208 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    static int N;
+    static int[] numArr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        InputHandler();
+
+        twoPointer();
+
+        printResult();
+    }
+
+    private static void printResult() {
+        System.out.println(sb.toString().trim());
+    }
+
+    private static void twoPointer() {
+        int left = 0;
+        int right = N - 1;
+        int leftNum = numArr[left];
+        int rightNum = numArr[right];
+        int closestSum = Integer.MAX_VALUE;
+
+        while (left < right) {
+            int sum = numArr[left] + numArr[right];
+
+            if (sum == 0) {
+                sb.append(numArr[left]).append(" ").append(numArr[right]).append("\n");
+                return;
+            }
+
+            if (Math.abs(sum) < closestSum) {
+                closestSum = Math.abs(sum);
+                leftNum = numArr[left];
+                rightNum = numArr[right];
+            }
+
+            if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        sb.append(leftNum).append(" ").append(rightNum).append("\n");
+    }
+
+    private static void InputHandler() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        numArr = new int[N];
+        for (int i = 0; i < N; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numArr);
+    }
+}
