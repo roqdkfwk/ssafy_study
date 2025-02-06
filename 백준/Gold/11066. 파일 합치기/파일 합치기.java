2356e0ac8e7f4f83b7f55bbb8fbedecb0b5bd080
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-02-06 22:37:49 +0900

    [Gold III] Title: 파일 합치기, Time: 432 ms, Memory: 29448 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int T;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;;
+
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int k;
+            int[] novel;
+            int[] sum;
+            int[][] dp;
+
+            k = Integer.parseInt(br.readLine());
+            novel = new int[k + 1];
+            dp = new int[k + 1][k + 1];
+            sum = new int[k + 1];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= k; i++) {
+                novel[i] = Integer.parseInt(st.nextToken());
+                sum[i] = sum[i - 1] + novel[i];
+            }
+
+            for (int n = 1; n <= k; n++) {
+                for (int from = 1; from + n <= k; from++) {
+                    int to = from + n;
+                    dp[from][to] = Integer.MAX_VALUE;
+                    for (int divide = from; divide < to; divide++) {
+                        dp[from][to] = Math.min(dp[from][to], dp[from][divide] + dp[divide + 1][to] + sum[to] - sum[from - 1]);
+                    }
+                }
+            }
+            System.out.println(dp[1][k]);
+        }
+    }
+}
