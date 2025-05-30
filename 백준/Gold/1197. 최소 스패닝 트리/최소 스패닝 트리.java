3ddbbc53a614a0f5a02101247b0c94d99f8859ec
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-05-30 23:57:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 456 ms, Memory: 47344 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,18 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-
-	static int V;	// 정점의 개수
-	static int E;	// 간선의 개수
-	static int[][] edges;	// 간선의 정보
-	static int[] p;	// 대표를 저장할 배열
-	static int ans;
-	static int count;
+	
+	static class Edge implements Comparable<Edge> {
+		int from, to, weight;
+		
+		public Edge(int from, int to, int weight) {
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Edge e) {
+			return Integer.compare(this.weight, e.weight);
+		}
+	}
+	
+	static int V, E;
+	static PriorityQueue<Edge> edges;;
+	static int[] parent;
+	static int answer;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,64 +30,53 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
-		edges = new int[E][3];
+		edges = new PriorityQueue<>();
 		for (int i = 0; i < E; i++) {
+ 			st = new StringTokenizer(br.readLine());
+ 			edges.add(
+ 					new Edge(Integer.parseInt(st.nextToken()),
+ 							Integer.parseInt(st.nextToken()),
+ 							Integer.parseInt(st.nextToken()))
+ 					);
+		}
+		
+		parent = new int[V + 1];
+		for (int i = 1; i<= V; i++) {
+			parent[i] = i;
+		}
+		
+		int count = 0;
+		while(!edges.isEmpty()) {
+			Edge curr = edges.poll();
 			
-			st = new StringTokenizer(br.readLine());
-			edges[i][0] = Integer.parseInt(st.nextToken());
-			edges[i][1] = Integer.parseInt(st.nextToken());
-			edges[i][2] = Integer.parseInt(st.nextToken());
-		}	// 간선의 정보를 입력
-		
-//		for (int[] edge : edges) {
-//			System.out.println(Arrays.toString(edge));
-//		}
-		
-		Arrays.sort(edges, new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				
-				return (o1[2] - o2[2]) > 0 ? 1 : -1;
-			}			
-		});	// edges배열을 가충치 기준 오름차순으로 정렬
-		
-//		for (int[] edge : edges) {
-//			System.out.println(Arrays.toString(edge));
-//		}
-		
-		ans = 0;
-		count = 0;
-		
-		p = new int[V + 1];
-		for (int i = 1; i < V + 1; i++) p[i] = i;
-		for (int i = 0; i < E; i++) { 
+			int A = findParent(curr.from);
+			int B = findParent(curr.to);
 			
-			int px = findset(edges[i][0]);
-			int py = findset(edges[i][1]);
+			if (A == B) continue;
 			
-			if (px != py) {
-				
-				union(px, py);
-				ans += edges[i][2];
-				count++;
+			union(A, B);
+			answer += curr.weight;
+			count++;
+			
+			if (count == V - 1) {
+				System.out.println(answer);
+				return;
 			}
-			
-			if (count == V - 1) break;
-		}	// 간선을 잇는 작업
-		
-		System.out.println(ans);
+		}
 	}
 
-	static void union(int x, int y) {
-		
-		p[findset(y)] = findset(x);
-	}	// union
-
-	static int findset(int x) {
-		
-		if (x != p[x])	// 대표자가 아니라면
-			p[x] = findset(p[x]);
-		return p[x];
-	}	// findset
+	private static int findParent(int x) {
+		if (parent[x] != x) {
+			parent[x] = findParent(parent[x]);
+		}
+		return parent[x];
+	}
+	
+	private static void union(int x, int y) {
+		if (x < y) {
+			parent[y] = parent[x];
+		} else {
+			parent[x] = parent[y];
+		}
+	}
 }
