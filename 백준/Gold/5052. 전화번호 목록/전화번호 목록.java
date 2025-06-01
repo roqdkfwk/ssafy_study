189a321cba9b0ea91afad63d7f0ff3c72f0de8f9
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-06-02 00:24:07 +0900

    [Gold IV] Title: 전화번호 목록, Time: 504 ms, Memory: 200944 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,39 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-
-    static int t, n;
-    static String[] str;
-    static PriorityQueue<String> pq;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        t = Integer.parseInt(br.readLine());
-        for (int T = 0; T < t; T++) {
-            n = Integer.parseInt(br.readLine());
-            str = new String[n];
-            for (int i = 0; i < n; i++) {
-                str[i] = br.readLine();
-            }
-
-            Arrays.sort(str);
-
-            boolean flag = false;
-            for (int i = 0; i < n - 1; i++) {
-                if (str[i + 1].startsWith(str[i])) {
-                    flag = true;
-                    break;
-                }
-            }
-            
-            sb.append(flag ? "NO" : "YES").append("\n");
-        }   // t에 대한 for문
-
-        System.out.println(sb);
-    }   // main
+	
+	static class Trie {
+		HashMap<Character, Trie> map = new HashMap<>();
+		boolean isEnd = false;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder answer = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			Map<Character, Trie> nums = new HashMap<>();
+			int N = Integer.parseInt(br.readLine());
+			boolean consistence = true;
+			
+			for (int i = 0; i < N; i++) {
+				String str = br.readLine();
+				
+				// 일관성 확인
+				if (availableNum(str, nums)) continue;
+				consistence = false;
+			}
+			
+			if (consistence) answer.append("YES").append("\n");
+			else answer.append("NO").append("\n");
+		}
+		
+		System.out.println(answer.toString().trim());
+	}
+	
+	private static boolean availableNum(String num, Map<Character, Trie> nums) {
+		boolean result = true;
+		for (int i = 0; i < num.length(); i++) {
+			char ch = num.charAt(i);
+			
+			if (nums.get(ch) == null) {
+				nums.put(ch, new Trie());
+				
+				// 마지막 숫자인 경우 isEnd를 true
+				if (i == num.length() - 1) {
+					nums.get(ch).isEnd = true;
+				}
+			} else {
+				if (nums.get(ch).isEnd) {
+					result = false;
+				}
+                
+                if (i == num.length() - 1) {
+					result = false;
+				}
+			}
+			
+			// 다음 문자를 탐색하기 위해서 nums에 새로운 map을 할당
+			nums = nums.get(ch).map;
+		}
+		return result;
+	}
 }
