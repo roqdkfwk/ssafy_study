13d65711ee887718d82ccd198b2758c91fa8fde5
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-06-04 23:55:55 +0900

    [Gold V] Title: 두 용액, Time: 392 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,66 +1,39 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-
-    static int N;
-    static int[] numArr;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
-        InputHandler();
-
-        twoPointer();
-
-        printResult();
-    }
-
-    private static void printResult() {
-        System.out.println(sb.toString().trim());
-    }
-
-    private static void twoPointer() {
-        int left = 0;
-        int right = N - 1;
-        int leftNum = numArr[left];
-        int rightNum = numArr[right];
-        int closetSum = Integer.MAX_VALUE;
-
-        while (left < right) {
-            int sum = numArr[left] + numArr[right];
-
-            if (sum == 0) {
-                sb.append(numArr[left]).append(" ").append(numArr[right]);
-                return;
-            }
-
-            if (Math.abs(sum) < closetSum) {
-                leftNum = numArr[left];
-                rightNum = numArr[right];
-                closetSum = Math.abs(sum);
-            }
-
-            if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        sb.append(leftNum).append(" ").append(rightNum);
-    }
-
-    private static void InputHandler() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine());
-
-        numArr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(numArr);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = N - 1;
+		int sum = Integer.MAX_VALUE;
+		String answer = "";
+		while (left < right) {
+			if (arr[left] + arr[right] == 0) {
+				System.out.println(arr[left] + " " + arr[right]);
+				return;
+			}
+			
+			if (sum > Math.abs(arr[left] + arr[right])) {
+				sum = Math.abs(arr[left] + arr[right]);
+				answer = arr[left] + " " + arr[right];
+			}
+			
+			if (arr[left] + arr[right] < 0) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		
+		System.out.println(answer);
+	}
 }
