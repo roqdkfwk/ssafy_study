79155600f487bd8d720624d1b203a70514338592
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-07-25 21:37:14 +0900

    [Platinum V] Title: 본대 산책2, Time: 192 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Platinum/12850. 본대 산책2/본대 산책2.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	
+	static Long[][] matrix;
+	static Map<Integer, Long[][]> matrixMap;
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		matrix = new Long[8][8];
+		for (int r = 0; r < 8; r++) {
+			Arrays.fill(matrix[r], 0L);
+		}
+		
+		matrix[0][1] = matrix[1][0] = 1L;
+		matrix[0][2] = matrix[2][0] = 1L;
+		matrix[1][2] = matrix[2][1] = 1L;
+		matrix[1][3] = matrix[3][1] = 1L;
+		matrix[2][3] = matrix[3][2] = 1L;
+		matrix[2][4] = matrix[4][2] = 1L;
+		matrix[3][4] = matrix[4][3] = 1L;
+		matrix[3][5] = matrix[5][3] = 1L;
+		matrix[4][5] = matrix[5][4] = 1L;
+		matrix[4][6] = matrix[6][4] = 1L;
+		matrix[5][7] = matrix[7][5] = 1L;
+		matrix[6][7] = matrix[7][6] = 1L;
+	
+		matrixMap = new HashMap<>();
+		int D = sc.nextInt();
+		exponent(D);
+		System.out.println(matrixMap.get(D)[0][0]);
+	}
+	
+	private static Long[][] exponent(int exp) {
+		if (exp == 1) {
+			return matrix;
+		}
+
+		if (matrixMap.containsKey(exp)) {
+			return matrixMap.get(exp);
+		}
+		
+		if (exp % 2 == 0) {
+			matrixMap.putIfAbsent((exp / 2), exponent(exp / 2));
+			matrixMap.put(exp, calculate(matrixMap.get(exp / 2), matrixMap.get(exp / 2)));
+			return matrixMap.get(exp);
+		}
+		
+		if (exp % 2 == 1) {
+			matrixMap.putIfAbsent((exp / 2), exponent(exp / 2));
+			matrixMap.putIfAbsent(exp - 1, calculate(matrixMap.get(exp / 2), matrixMap.get(exp / 2)));
+			matrixMap.put(exp, calculate(matrixMap.get(exp - 1), matrix));
+			return matrixMap.get(exp);
+		}
+		
+		return null;
+	}
+	
+	private static Long[][] calculate(Long[][] m1, Long[][] m2) {
+		Long[][] resultMatrix = new Long[8][8];
+		for (int r = 0; r < 8; r++) {
+			Arrays.fill(resultMatrix[r], 0L);
+		}
+		
+		for (int r = 0; r < 8; r++) {
+			
+			for (int c = 0; c < 8; c++) {
+				Long num = 0L;
+				for (int i = 0; i < 8; i++) {
+					num += ((m1[r][i] * m2[i][c]) % 1_000_000_007);
+				}
+				resultMatrix[r][c] = (num % 1_000_000_007);
+			}
+		}
+		
+		return resultMatrix;
+	}
+}
