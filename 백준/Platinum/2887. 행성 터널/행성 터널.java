e2868f263ffb8831c6870acbbf4b08fd5f6ce31f
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-08-06 02:32:48 +0900

    [Platinum V] Title: 행성 터널, Time: 1592 ms, Memory: 70464 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -1,133 +1,120 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 	
-	static class Vertex {
+	static class Star {
 		int num, x, y, z;
 		
-		Vertex(int num, int x, int y, int z) {
+		public Star(int num, int x, int y, int z) {
 			this.num = num;
 			this.x = x;
 			this.y = y;
 			this.z = z;
 		}
-	}	// Vertex
+	}
 	
 	static class Edge implements Comparable<Edge> {
+		int num1, num2, length;
 		
-		int start, end, weight;
-		
-		Edge(int start, int end, int weight) {
-			this.start = start;
-			this.end = end;
-			this.weight = weight;
+		public Edge(int num1, int num2, int length) {
+			this.num1 = num1;
+			this.num2 = num2;
+			this.length = length;
 		}
 		
 		@Override
-		public int compareTo(Edge o) {
-			return weight - o.weight;
+		public int compareTo(Edge e) {
+			return Integer.compare(this.length, e.length);
 		}
-	}	// Edge
+	}
 	
-	static int N;	// 행성의 개수
-	static Vertex[] vertex;	// 정점 배열
-	static ArrayList<Edge> edgeList;	// 간선 정보
-	static int[] p;
-	static int ans;	// 최소비용
-	static int count;	// 간선의 개수
+	static int N;
+	static Star[] stars;
+	static PriorityQueue<Edge> pq;
+	static Edge[] sortedByX, sortedByY, sortedByZ;
+	static int[] parent;
+	static long answer;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		vertex = new Vertex[N];
-				
+		stars = new Star[N];
 		for (int i = 0; i < N; i++) {
-			
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int z = Integer.parseInt(st.nextToken());
-			
-			vertex[i] = new Vertex(i, x, y, z);
+			stars[i] = new Star(
+					i, Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken()));
 		}
 		
-		edgeList = new ArrayList<>();
+		// x, y, z에 대해서 정렬하면 인접한 Star 사이의 x, y, z 거리가 가장 가까운 상태
+		// 해당 Edge를 모두 pq에 넣은 후 가장 짧은 (N-1)개의 Edge를 사용해서 연결
+		pq = new PriorityQueue<>();
 		
-		// x에 대해 정렬
-		Arrays.sort(vertex, (v1, v2) -> v1.x - v2.x);
+		Arrays.sort(stars, (a, b) -> a.x - b.x);
 		for (int i = 0; i < N - 1; i++) {
-			
-			// 비용을 계산
-			int weight = Math.abs(vertex[i].x - vertex[i + 1].x);
-			
-			// 두 정점의 정보와 비용을 edgeList에 저장
-			edgeList.add(new Edge(vertex[i].num, vertex[i + 1].num, weight));
+			Star s1 = stars[i];
+			Star s2 = stars[i + 1];
+			pq.add(new Edge(s1.num, s2.num, s2.x - s1.x));
 		}
 		
-		// y에 대해 정렬
-		Arrays.sort(vertex, (v1, v2) -> v1.y - v2.y);
+		Arrays.sort(stars, (a, b) -> a.y - b.y);
 		for (int i = 0; i < N - 1; i++) {
-			
-			int weight = Math.abs(vertex[i].y - vertex[i + 1].y);
-			edgeList.add(new Edge(vertex[i].num, vertex[i + 1].num, weight));
+			Star s1 = stars[i];
+			Star s2 = stars[i + 1];
+			pq.add(new Edge(s1.num, s2.num, s2.y - s1.y));
 		}
 		
-		// z에 대해 정렬
-		Arrays.sort(vertex, (v1, v2) -> v1.z - v2.z);
+		Arrays.sort(stars, (a, b) -> a.z - b.z);
 		for (int i = 0; i < N - 1; i++) {
-			
-			int weight = Math.abs(vertex[i].z - vertex[i + 1].z);
-			edgeList.add(new Edge(vertex[i].num, vertex[i + 1].num, weight));
+			Star s1 = stars[i];
+			Star s2 = stars[i + 1];
+			pq.add(new Edge(s1.num, s2.num, s2.z - s1.z));
 		}
 		
-		// 각 정점의 부모를 자기 자신으로 설정
-		p = new int[N];
-		for (int i = 0; i < N; i++) p[i] = i;
+		combineStars();
 		
-		// edgeList를 weight 기준 오름차순으로 정렬
-		Collections.sort(edgeList);
-		
-		ans = 0;
-		count = 0;
-		for (int i = 0; i < edgeList.size(); i++) {
-			
-			Edge edge = edgeList.get(i);
-			
-			int px = findset(edge.start);
-			int py = findset(edge.end);
-			
-			// px != py이면 서로소 집합이므로 두 집합을 union
-			if (px != py) {
-				
-				union(px, py);	// 두 집합을 union하고
-				ans += edge.weight;	// ans에 필요한 비용을 더한 후
-				count++;	// 간선의 개수를 1만큼 증가
-			}
-			
-			// 정점의 개수가 N개이므로 간선의 개수가 (N - 1)개이면 MST완성이므로 반복문 종료
-			if (count == N - 1) break;
-		}
-		
-		System.out.println(ans);
-	}	// main
-	
-	static void union(int x, int y) {
-	
-		p[findset(y)] = findset(x);
+		System.out.println(answer);
 	}
-
-	static int findset(int x) {
 	
-		if (x != p[x])
-			p[x] = findset(p[x]);
-		return p[x];
-	}	// findset
+	private static void combineStars() {
+		parent = new int[N];
+		for (int i = 0; i < N; i++) {
+			parent[i] = i;
+		}
+		
+		int edges = 0;
+		while (edges < N - 1) {
+			Edge curr = pq.poll();
+			
+			int num1 = findParent(curr.num1);
+			int num2 = findParent(curr.num2);
+			
+			// 이미 연결된 경우
+			if (num1 == num2) continue;
+			
+			// 연결되지 않은 경우 연결
+			union(num1, num2);
+			answer += curr.length;
+			edges++;
+		}
+	}
+	
+	private static void union(int num1, int num2) {
+		// 번호가 작은 Star를 부모로
+		if (num1 < num2) {
+			parent[num2] = num1;
+		} else {
+			parent[num1] = num2;
+		}
+	}
+	
+	private static int findParent(int num) {
+		if (num != parent[num]) {
+			parent[num] = findParent(parent[num]);
+		}
+		return parent[num];
+	}
 }
