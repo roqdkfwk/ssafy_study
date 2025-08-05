91d904a3e8fc37090e6f4ef4e262f5d67a8a3ba2
Seokmin  Yoo <146637064+roqdkfwk@users.noreply.github.com>
2025-08-05 16:11:31 +0900

    [Gold III] Title: 개미굴, Time: 176 ms, Memory: 15584 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -1,50 +1,47 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-
-    static int N;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException, NumberFormatException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 먹이 정보를 사전 순으로 출력하기 위해서 TreeMap 사용
-        TreeMap<String, TreeMap> root = new TreeMap<>();
-        N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            String[] foods = br.readLine().split(" ");
-            
-            addRoot(root, foods, 1);
-        }
-
-        for (String food : root.keySet()) {
-            printRoot(root, food, 0);
-        }
-
-        System.out.println(sb.toString().trim());
-    }
-
-    // addRoot(층 정보, 먹이 정보, 다음 먹이의 index)
-    private static void addRoot(TreeMap<String, TreeMap> food, String[] foods, int index) {
-        if (index >= foods.length) return;
-
-        if (!food.containsKey(foods[index])) {
-            food.put(foods[index], new TreeMap<>());
-        }
-        addRoot(food.get(foods[index]), foods, index + 1);
-    }
-
-    private static void printRoot(TreeMap<String, TreeMap> food, String str, int depth) {
-        for (int i = 0; i < depth; i++) {
-            sb.append("--");
-        }
-        sb.append(str).append("\n");
-
-        TreeMap<String, TreeMap> nextFoods = food.get(str);
-        if (nextFoods == null || nextFoods.isEmpty()) return;
-
-        for (String nextFood : nextFoods.keySet()) {
-            printRoot(nextFoods, nextFood, depth + 1);
-        }
-    }
+	
+	static int N;
+	static StringBuilder answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		TreeMap<String, TreeMap> root = new TreeMap<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int T = Integer.parseInt(st.nextToken());
+			
+			TreeMap<String, TreeMap> map = root;
+			for (int t = 0; t < T; t++) {
+				String key = st.nextToken();
+				map.putIfAbsent(key, new TreeMap<String, TreeMap>());
+				map = map.get(key);
+			}
+		}
+		
+		answer = new StringBuilder();
+		for (String key : root.keySet()) {
+			visualize(key, root, 0);
+		}
+		
+		System.out.println(answer.toString().trim());
+	}
+	
+	private static void visualize(String keyword, TreeMap<String, TreeMap> map, int depth) {
+		for (int i = 0; i < depth; i++) {
+			answer.append("--");
+		}
+		answer.append(keyword).append("\n");
+		
+		TreeMap<String, TreeMap> nextMap = map.get(keyword);
+		if (nextMap == null || nextMap.isEmpty()) return;
+		
+		for (String nextKey : nextMap.keySet()) {
+			visualize(nextKey, nextMap, depth + 1);
+		}
+	}
 }
